bff75c89d527619dba0a0b86abbfc6bdef276b95
CeeDeeBee <cdbarnes98@gmail.com>
2020-07-17 11:52:45 -0400

    Add ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.insert_index = 0
+        self.storage = []
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            self.storage[self.insert_index] = item
+            if self.insert_index + 1 == self.capacity:
+                self.insert_index = 0
+            else:
+                self.insert_index += 1
+        else:
+            self.storage.append(item)
 
     def get(self):
-        pass
+        return self.storage
