306ca571c3111836ae800fe0912bc1c52a12f20b
CeeDeeBee <cdbarnes98@gmail.com>
2020-07-17 12:41:24 -0400

    Add reverse list solution

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,9 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node:
+            next_node = node.next_node
+            node.next_node = prev
+            self.reverse_list(next_node, node)
+        else:
+            self.head = prev
